d81f64afebc0116bd3d51356e63a7fab6a31255a
Huy Tran <huytran172@gmail.com>
2024-09-22 15:12:46 -0400

    Fix pan overlay

--- frontend/src/components/notes/NoteLayer.vue
+++ frontend/src/components/notes/NoteLayer.vue
@@ -10,23 +10,9 @@ const strokeColor = ref('rgba(255, 255, 255, 0.4)')
 const fillColor = ref('#FFFFFF')
 const radius = ref(10)
 
-/**
- * @type {import('vue').Ref<{ select: import('ol/interaction').Select } | null>}
- */
-const selectNoteInteractionRef = ref(null)
-
-/**
- * @type {import('vue').Ref<{ source: import('ol/source/Vector').default } | null>}
- */
-const noteSourceRef = ref(null)
-
-/**
- * @type {import('vue').Ref<{ overlay: import('ol/Overlay').default } | null>}
- */
-const noteOverlayRef = ref(null)
-
 const noteStore = useNoteStore()
-const { displayedNotes, selectedNote } = storeToRefs(noteStore)
+const { displayedNotes, selectedNote, selectNoteInteractionRef, noteSourceRef, noteOverlayRef } =
+  storeToRefs(noteStore)
 const { resetNoteOverlay } = noteStore
 
 const rightPanelStore = useRightPanelStore()
@@ -46,20 +32,21 @@ function featureSelected(event) {
   }
 
   selectedNote.value = features[0].getProperties()
-  console.log(selectedNote.value)
 }
 
+// Set position for the overlay on the map
 watch(selectedNote, (value) => {
   if (value) {
-    noteOverlayRef.value.overlay.setPosition(value.geom.coordinates)
+    noteOverlayRef.value.setPosition(value.geom.coordinates)
   } else {
     selectNoteInteractionRef.value.select.getFeatures().clear()
+    noteOverlayRef.value.setPosition(null)
   }
 })
 </script>
 
 <template>
-  <ol-vector-layer v-if="activePanel === RIGHT_PANELS.NOTE">
+  <ol-vector-layer v-if="true">
     <ol-source-vector ref="noteSourceRef">
       <ol-feature v-for="data in displayedNotes" :properties="data" :key="data.id">
         <ol-geom-point :coordinates="data.geom.coordinates"></ol-geom-point>
@@ -82,14 +69,10 @@ watch(selectedNote, (value) => {
     </ol-style>
   </ol-interaction-select>
 
-  <ol-overlay :auto-pan="true" ref="noteOverlayRef" :stopEvent="true">
-    <template v-if="selectedNote && activePanel === RIGHT_PANELS.NOTE">
-      <div
-        class="absolute -top-6 left-8 min-w-[350px] max-w-sm cursor-default rounded bg-white shadow-[0px_0px_6px_4px_#0000001F]"
-      >
-        <MapNoteOverlay />
-      </div>
-      <div class="absolute -top-2.5 left-6 h-4 w-4 rotate-45 bg-white"></div>
-    </template>
+  <ol-overlay :autoPan="true" ref="noteOverlayRef" :stopEvent="true" :offset="[32, -16]">
+    <div class="rounded bg-white shadow-[0px_0px_6px_4px_#0000001F]">
+      <MapNoteOverlay />
+      <div class="absolute h-4 w-4 top-3 -left-2 rotate-45 bg-white"></div>
+    </div>
   </ol-overlay>
 </template>
